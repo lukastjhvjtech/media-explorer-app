65d47da1fd82173b9ffe71af6863f418fe7067f2
lukastjhvjtech <lukas.freith@ohs-hh.de>
2026-04-22 22:02:04 +0200

    Add MediaItem class with properties and constructor

--- lib/models/media_item.dart
+++ lib/models/media_item.dart
@@ -1,1 +1,18 @@
+// lib/models/media_item.dart
+    class MediaItem {
+      final String id;
+      final String path;
+      final String type; // 'image' oder 'video'
+      final DateTime dateModified;
+      final bool isLocal; // True für lokal, False für remote
+      final String? displayName; // Optionaler Anzeigename
 
+      MediaItem({
+        required this.id,
+        required this.path,
+        required this.type,
+        required this.dateModified,
+        required this.isLocal,
+        this.displayName,
+      });
+    }

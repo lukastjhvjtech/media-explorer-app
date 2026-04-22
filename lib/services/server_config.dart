848d9eb6d6966fd44687af293a56d2336aae42bf
lukastjhvjtech <lukas.freith@ohs-hh.de>
2026-04-22 22:02:42 +0200

    Add ServerConfig class with server credentials

--- lib/services/server_config.dart
+++ lib/services/server_config.dart
@@ -1,1 +1,6 @@
-
+// lib/services/server_config.dart
+    class ServerConfig {
+      static const String baseUrl = 'https://dein-server-url.com/webdav'; // Passe an
+      static const String username = 'dein_benutzername';
+      static const String password = 'dein_passwort';
+    }
